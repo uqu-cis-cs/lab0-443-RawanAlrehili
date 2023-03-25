1f13a1bafee4424838dc7cc4a27fb54cc5fce25b
RawanAlrehili <120566686+RawanAlrehili@users.noreply.github.com>
2023-03-25 20:10:58 +0000

    lab0

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -23,7 +23,29 @@ public class App{
 *        1 Tweetle 3 TweetleBeetle 5 TweetlePoodle 7 TweetleBeetle 9 Tweetle 11
 *        TweetleBeetlePoodle 13 Tweetle 15 TweetleBeetle 17 TweetlePoodle 19 TweetleBeetle 
 */
+   twisters();
+        String str = "AEFED";
+        phonekeyPad(str);
+    
+    public static void twisters(){
+        for(int i=1; i<=110; i++){
+        if(i%6==0){
+            System.out.print("Poodle");
+        }else if(i%4==0){
+            System.out.print("Beetle");
+        }else if(i%2==0){
+            System.out.print("Tweetle");
+        }else{
+            System.out.print(i+" ");
+        }
+        if(i%11==0){
+            System.out.println();
+        }
+    }
+    }
+    
 
+   
 /**
 * Write a method named "phoneKeypad" that takes a string parameter (str)
 * then it replaces each letter in (str) by a number that represnts it 
@@ -42,15 +64,38 @@ public class App{
 * Output: 2255464228626
 *
 */
- 
+  public static void phonekeyPad(String str){
+        for(int i=0; i<str.length(); i++){
+            char upper = Character.toUpperCase(str.charAt(i));
+            switch(upper){
+                case 'A':
+                case 'B':
+                case 'C':
+               System.out.print(2);
+               break;
+               case 'E':
+                case 'F':
+                case 'D':
+               System.out.print(3);
+               break;
+                default:
+                    System.out.println("this is not an alphabet");
+                    break;
+               
+            }
+        }
+    }
+}
+    
+
     public static void main(String [] args) {
         
 
         /* Write your code here */
      
-       //call method twisters()
-       //prompt user to enter a string 
-       //call method phoneKeypad(string)
+       call method twisters()
+       prompt user to enter a string 
+       call method phoneKeypad(string)
 
 
     }
